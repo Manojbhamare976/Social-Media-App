c407762dc0ea0fb5992c70d0030f22e7264b9c4e
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-21 09:46:03 +0530

    Added post routes to the server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import postRoutes from "./Routes/postRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//post routes
+app.use("/post", postRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
