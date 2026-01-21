5091de363d8013429f562913361ff32191bfb0ab
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-21 09:22:00 +0530

    Created comment model

---
+++ Backend/Models/comment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    text: {
+      type: "String",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
