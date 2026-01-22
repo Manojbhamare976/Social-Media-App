806ff4d594e8cec1334870ca5d30f13b3d306896
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-22 09:11:18 +0530

    Created user model

---
+++ Backend/Models/user.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.model({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    profilePic: {
+        type: String,
+        default: /images/profilePic
+    },
+    bio: {
+        type: String
+    },
+    followers: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    following: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true});
+
+const User = mongoose.model("User",userSchema);
+export default User;
