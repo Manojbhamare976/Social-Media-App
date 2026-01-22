054a116101f28e182f90c19165b8a16c64bd93b3
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-22 09:01:46 +0530

    Created post model

---
+++ Backend/Models/post.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    caption: {
+      type: String,
+    },
+    content: {
+      type: [String],
+    },
+    likes: {
+      type: Number,
+    },
+    comments: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  },
+  { timestamps: true },
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
