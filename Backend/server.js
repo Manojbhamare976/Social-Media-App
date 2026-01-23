450d83c14806c49153c8a27a42d425aaf967ccf8
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-23 13:11:12 +0530

    Added user auth routes in server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import postRoutes from "./Routes/postRoutes.js";
+import userAuthRoutes from "./Routes/userAuthRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 dotenv.config();
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //post routes
 app.use("/post", postRoutes);
-
+//user auth routes
+app.use("/user", userAuthRoutes);
 app.listen(3000, () => console.log("Server running on port 3000"));
