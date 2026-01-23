521fd7e59d2593b574d68be19429f442b8f8b261
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-23 22:27:21 +0530

    Added cookie parser middleware

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import postRoutes from "./Routes/postRoutes.js";
 import userAuthRoutes from "./Routes/userAuthRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 //DB connection
 connectDB();
@@ -13,8 +15,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 //post routes
 app.use("/post", postRoutes);
+
 //user auth routes
 app.use("/user", userAuthRoutes);
+
 app.listen(3000, () => console.log("Server running on port 3000"));
