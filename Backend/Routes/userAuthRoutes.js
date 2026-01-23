f1d9daa6ca0237d1cab0afeeb3d075d8ff031af7
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-23 22:36:47 +0530

    Created user logout route

--- Backend/Routes/userAuthRoutes.js
+++ Backend/Routes/userAuthRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signup, login } from "../Controllers/userAuth.js";
+import { signup, login, logout } from "../Controllers/userAuth.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.put("/logout", logout);
 export default router;
