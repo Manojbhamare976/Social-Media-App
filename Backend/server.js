01f068afdd2f1bea289aa393040823c814a8a06a
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-24 18:18:37 +0530

    Imported and used comment routes

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import postRoutes from "./Routes/postRoutes.js";
 import userAuthRoutes from "./Routes/userAuthRoutes.js";
+import commentRoutes from "./Routes/commentRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 import cookieParser from "cookie-parser";
@@ -22,5 +23,8 @@ app.use("/post", postRoutes);
 
 //user auth routes
 app.use("/user", userAuthRoutes);
+
+//comment routes
+app.use("/comment", commentRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
