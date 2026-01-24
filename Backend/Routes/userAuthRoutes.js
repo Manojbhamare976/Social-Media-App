6e0f56a945e150cfce2a54105088a4f891f828a4
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-24 16:02:46 +0530

    Imported and used the loginLimiter middleware

--- Backend/Routes/userAuthRoutes.js
+++ Backend/Routes/userAuthRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { signup, login, logout } from "../Controllers/userAuth.js";
+import {
+  signup,
+  login,
+  logout,
+  loginLimiter,
+} from "../Controllers/userAuth.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login", loginLimiter, login);
 router.put("/logout", logout);
 export default router;
