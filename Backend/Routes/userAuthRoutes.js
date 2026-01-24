56facee32e198261aeaf143d821881c7396d0707
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-24 16:03:35 +0530

    Created refresh route

--- Backend/Routes/userAuthRoutes.js
+++ Backend/Routes/userAuthRoutes.js
@@ -4,10 +4,12 @@ import {
   login,
   logout,
   loginLimiter,
+  refresh,
 } from "../Controllers/userAuth.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", loginLimiter, login);
+router.get("/refresh", refresh);
 router.put("/logout", logout);
 export default router;
