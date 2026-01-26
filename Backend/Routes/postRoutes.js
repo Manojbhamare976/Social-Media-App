c78138afb6aaa2c40bc2e14dbf1ff69ed5daa0ac
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-26 20:30:24 +0530

    Feature -  Created get route for posts

--- Backend/Routes/postRoutes.js
+++ Backend/Routes/postRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { createPost, deletePost } from "../Controllers/postController.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+} from "../Controllers/postController.js";
 const router = express.Router();
 
 router.post("/create", createPost);
 router.delete("/delete", deletePost);
+router.get("/find", getPost);
+
 export default router;
