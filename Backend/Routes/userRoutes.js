b518d43b890385f3b65f96f1c851b09a66ed7a62
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-28 18:53:31 +0530

    feat: create put route

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   increaseFollowers,
   isFollowing,
+  decreaseFollowers,
 } from "../Controllers/userController.js";
 import { authenticateUser } from "../Controllers/userAuth.js";
 const router = express.Router();
 
 router.post("/increase/followers", authenticateUser, increaseFollowers);
 router.get("/isfollowing", authenticateUser, isFollowing);
+router.put("/decrease/followers", authenticateUser, decreaseFollowers);
 
 export default router;
