79bd62a234209f13e2626412e4aba3b37bd8bc7c
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-29 21:17:01 +0530

    refactor: change likes field to array

--- Backend/Models/post.js
+++ Backend/Models/post.js
@@ -12,8 +12,15 @@ const postSchema = new mongoose.Schema(
     content: {
       type: [String],
     },
-    likes: {
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    likesCount: {
       type: Number,
+      default: 0,
     },
     comments: {
       type: mongoose.Schema.Types.ObjectId,
