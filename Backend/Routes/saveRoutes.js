889227d28ad28c31e565670146e03048c6cf9cfa
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-30 22:53:33 +0530

    feat: create put route to unsave post

--- Backend/Routes/saveRoutes.js
+++ Backend/Routes/saveRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { authenticateUser } from "../Controllers/userAuth.js";
-import { savePost } from "../Controllers/saveController.js";
+import { savePost, unsavePost } from "../Controllers/saveController.js";
 
 const router = express.Router();
 
 router.post("/save", authenticateUser, savePost);
+router.put("/unsave", authenticateUser, unsavePost);
 
 export default router;
