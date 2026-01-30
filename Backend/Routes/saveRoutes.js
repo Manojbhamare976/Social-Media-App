5220b39e495351f0d852eafbd3d71d8af3cfb80b
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-30 23:35:54 +0530

    feat: create get route for issaved

--- Backend/Routes/saveRoutes.js
+++ Backend/Routes/saveRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { authenticateUser } from "../Controllers/userAuth.js";
-import { savePost, unsavePost } from "../Controllers/saveController.js";
+import {
+  savePost,
+  unsavePost,
+  isSaved,
+} from "../Controllers/saveController.js";
 
 const router = express.Router();
 
 router.post("/save", authenticateUser, savePost);
 router.put("/unsave", authenticateUser, unsavePost);
+router.get("/issaved", authenticateUser, isSaved);
 
 export default router;
