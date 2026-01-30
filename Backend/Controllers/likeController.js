371e604041d2a730202f8cd44472a91e318461cf
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-30 21:25:54 +0530

    feat: create isLiked function

--- Backend/Controllers/likeController.js
+++ Backend/Controllers/likeController.js
@@ -41,26 +41,47 @@ async function dislike(req, res) {
     return console.log("postId not found");
   }
 
-  let userLikedPost = user.likedPosts.inlcudes(post._id);
+  let userLikedPost = user.likedPosts.includes(post._id);
   if (!userLikedPost) {
-    return console.log("user has not liked this post");
+    console.log("user has not liked this post");
+    return;
   } else {
     console.log("user has liked this post");
   }
 
-  let postLikes = post.postLikes.inlcudes(user._id);
+  let postLikes = post.likes.includes(user._id);
   if (!postLikes) {
-    return console.log("user has not liked this post");
+    console.log("user has not liked this post");
+    return;
   } else {
     console.log("this post has been liked by a user");
   }
 
-  user.likedPosts = user.likedPosts.filter(post._id);
+  user.likedPosts = user.likedPosts.filter((id) => id.toString() !== postId);
+  console.log(user.likedPosts);
   await user.save();
 
-  post.likes = post.likes.filter(user._id);
+  post.likes = post.likes.filter((id) => id.toString() !== userId);
   post.likesCount -= 1;
+  console.log(post.likes);
   await post.save();
 }
 
-export { like, dislike };
+async function isLiked(req, res) {
+  let { userId } = req.user;
+  let { postId } = req.query;
+
+  let user = await User.findById(userId);
+  let post = await Post.findById(postId);
+
+  if (!user) {
+    return res.json({ msg: "user not found" });
+  } else if (!post) {
+    return res.json({ msg: "post not found" });
+  }
+
+  let likedPost = user.likedPosts.includes(post._id);
+  return res.json({ likedPost });
+}
+
+export { like, dislike, isLiked };
