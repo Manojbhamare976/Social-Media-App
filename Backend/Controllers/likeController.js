29d24818d5fa5b4955c2613dddf78b95e71aedee
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-30 19:54:12 +0530

    feat: add dislike posts functionality

--- Backend/Controllers/likeController.js
+++ Backend/Controllers/likeController.js
@@ -28,4 +28,39 @@ async function like(req, res) {
   }
 }
 
-export { like };
+async function dislike(req, res) {
+  let { userId } = req.user;
+  let { postId } = req.body;
+
+  let user = await User.findById(userId);
+  let post = await Post.findById(postId);
+
+  if (!user) {
+    return console.log("userId not found");
+  } else if (!post) {
+    return console.log("postId not found");
+  }
+
+  let userLikedPost = user.likedPosts.inlcudes(post._id);
+  if (!userLikedPost) {
+    return console.log("user has not liked this post");
+  } else {
+    console.log("user has liked this post");
+  }
+
+  let postLikes = post.postLikes.inlcudes(user._id);
+  if (!postLikes) {
+    return console.log("user has not liked this post");
+  } else {
+    console.log("this post has been liked by a user");
+  }
+
+  user.likedPosts = user.likedPosts.filter(post._id);
+  await user.save();
+
+  post.likes = post.likes.filter(user._id);
+  post.likesCount -= 1;
+  await post.save();
+}
+
+export { like, dislike };
