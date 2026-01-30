083e656905f44f0406cad85c8e52f45f9384256f
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-30 21:26:34 +0530

    feat: create get route for isliked

--- Backend/Routes/likeRoutes.js
+++ Backend/Routes/likeRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { like, dislike } from "../Controllers/likeController.js";
+import { like, dislike, isLiked } from "../Controllers/likeController.js";
 import { authenticateUser } from "../Controllers/userAuth.js";
 const router = express.Router();
 
 router.post("/like", authenticateUser, like);
 router.put("/dislike", authenticateUser, dislike);
+router.get("/isliked", authenticateUser, isLiked);
 
 export default router;
