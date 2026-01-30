34a6780dbd868e67d1a8ab6b2a9816b07b072f1c
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-30 23:35:10 +0530

    feat: create is saved function which checks if a post is saved by the user

--- Backend/Controllers/saveController.js
+++ Backend/Controllers/saveController.js
@@ -19,18 +19,42 @@ async function savePost(req, res) {
 }
 
 async function unsavePost(req, res) {
-  let { userId } = req.user;
-  let { postId } = req.body;
+  try {
+    let { userId } = req.user;
+    let { postId } = req.body;
 
-  let user = User.findById(userId);
-  let userSavedPosts = await user.savedPosts.includes(postId);
-  if (!userSavedPosts) {
-    return res.status(400).json({ msg: "user hasn't saved this post" });
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ msg: "user not found" });
+    }
+    let userSavedPosts = user.savedPosts.includes(postId);
+    if (!userSavedPosts) {
+      return res.status(400).json({ msg: "user hasn't saved this post" });
+    }
+    user.savedPosts = await user.savedPosts.filter((id) => {
+      id !== postId;
+    });
+    await user.save();
+  } catch (err) {
+    console.log(err.message);
   }
-  user.savedPosts = await user.savedPosts.filter((id) => {
-    id !== postId;
-  });
-  await user.save();
 }
 
-export { savePost, unsavePost };
+async function isSaved(req, res) {
+  let { userId } = req.user;
+  let { postId } = req.query;
+
+  let user = await User.findById(userId);
+  if (!user) {
+    return res.status(400).json({ msg: "user not found" });
+  }
+
+  let userSavedPosts = user.savedPosts.includes(postId);
+  console.log(`userSavedPosts = ${userSavedPosts}`);
+  if (!userSavedPosts) {
+    return res.json({ userSavedPosts });
+  }
+  return res.json({ userSavedPosts });
+}
+
+export { savePost, unsavePost, isSaved };
