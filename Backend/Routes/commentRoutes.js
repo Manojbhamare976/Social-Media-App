cd9b1b6a57a53a39879137e2fe3d39694fd14fe4
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-01-31 14:21:57 +0530

    feat: create get route for showComments

--- Backend/Routes/commentRoutes.js
+++ Backend/Routes/commentRoutes.js
@@ -3,6 +3,7 @@ import express from "express";
 import {
   createComment,
   deleteComment,
+  showComments,
 } from "../Controllers/commentController.js";
 import { authenticateUser } from "../Controllers/userAuth.js";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.post("/create", authenticateUser, createComment);
 router.delete("/delete", authenticateUser, deleteComment);
+router.get("/comments", authenticateUser, showComments);
 
 export default router;
