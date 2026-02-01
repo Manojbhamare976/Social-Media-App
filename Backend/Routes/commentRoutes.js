6a1332e72f4f4f104e547b2c4330b2a2b0166dbb
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-01 23:53:59 +0530

    feat: create get route for showreply

--- Backend/Routes/commentRoutes.js
+++ Backend/Routes/commentRoutes.js
@@ -5,6 +5,7 @@ import {
   deleteComment,
   showComments,
   reply,
+  showReply,
 } from "../Controllers/commentController.js";
 import { authenticateUser } from "../Controllers/userAuth.js";
 
@@ -14,5 +15,6 @@ router.post("/create", authenticateUser, createComment);
 router.delete("/delete", authenticateUser, deleteComment);
 router.get("/comments", authenticateUser, showComments);
 router.post("/reply", authenticateUser, reply);
+router.get("/showreply", authenticateUser, showReply);
 
 export default router;
