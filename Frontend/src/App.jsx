bf94f7b61872c3afc68494b3647ff54e1dd8264f
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-04 16:33:04 +0530

    refactor: import and use Homepage and CreateComment components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Signup from "./userAuth/Signup.jsx";
+import Login from "./userAuth/Login.jsx";
+import Logout from "./userAuth/Logout.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import CreateComment from "./pages/CreateComment.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Signup />
+      <Login />
+      <Logout />
+      <Homepage />
+      <CreateComment postId={"697b8dfa5e3494282d76eb3e"} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
