1002215a352503acf46047af257406e50537b011
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-08 23:33:47 +0530

    feat: add route for user search page

--- Frontend/src/app-routes/routes.jsx
+++ Frontend/src/app-routes/routes.jsx
@@ -1,11 +1,13 @@
 import Homepage from "../pages/Homepage.jsx";
 import CreatePost from "../pages/CreatePost.jsx";
 import CreateComment from "../pages/CreateComment.jsx";
+import SearchUser from "../pages/Search.jsx";
 
 const routes = [
   { path: "/", element: <Homepage /> },
   { path: "/post/create", element: <CreatePost /> },
   { path: "/comment", element: <CreateComment /> },
+  { path: "/find/user", element: <SearchUser /> },
 ];
 
 export default routes;
