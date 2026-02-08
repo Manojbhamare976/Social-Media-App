af3057e17357814a1cf28e7a385766be27a3d39c
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-08 23:26:43 +0530

    feat: add find user by username route

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   increaseFollowers,
   isFollowing,
   decreaseFollowers,
+  findUser,
 } from "../Controllers/userController.js";
 import { authenticateUser } from "../Controllers/userAuth.js";
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/increase/followers", authenticateUser, increaseFollowers);
 router.get("/isfollowing", authenticateUser, isFollowing);
 router.put("/decrease/followers", authenticateUser, decreaseFollowers);
+router.get("/find/user", authenticateUser, findUser);
 
 export default router;
