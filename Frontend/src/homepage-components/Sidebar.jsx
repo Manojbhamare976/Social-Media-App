da7636a5fd4c39a3cf75daa45a1f17f318abdf7b
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-08 23:37:25 +0530

    feat: add navigation to user search page

--- Frontend/src/homepage-components/Sidebar.jsx
+++ Frontend/src/homepage-components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { House, Play, Search, Plus, CircleUserRound, Menu } from "lucide-react";
+import { House, Search, Plus, CircleUserRound, Menu } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import AppRoutes from "../app-routes/AppRoutes.jsx";
 
@@ -10,13 +10,18 @@ export default function Sidebar() {
     <>
       <button
         onClick={() => {
-          navigate("/home");
+          navigate("/");
         }}
       >
         <House />
       </button>
-      <Play />
-      <Search />
+      <button
+        onClick={() => {
+          navigate("/find/user");
+        }}
+      >
+        <Search />
+      </button>
       <Plus />
       <CircleUserRound />
       <Menu />
