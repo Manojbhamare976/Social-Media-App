c7bcc5fabefa90364cc92d4fe9e678967ad8cd4c
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-10 22:03:13 +0530

    feat: add delete post functionality

--- Backend/Controllers/postController.js
+++ Backend/Controllers/postController.js
@@ -1,5 +1,6 @@
 import Post from "../Models/post.js";
 import User from "../Models/user.js";
+import cloudinary from "../config/cloudinary.js";
 
 async function createPost(req, res) {
   try {
@@ -18,9 +19,9 @@ async function createPost(req, res) {
 
     const content = req.files.map((file) => file.path);
     const publicId = req.files.map((file) => file.filename);
-    const resourceType = req.files.map((file) => {
-      file.mimetype.startsWith("video") ? "video" : "image";
-    });
+    const resourceType = req.files.map((file) =>
+      file.mimetype.startsWith("video") ? "video" : "image",
+    );
 
     let post = new Post({
       user: userId,
@@ -46,12 +47,46 @@ async function createPost(req, res) {
 }
 
 async function deletePost(req, res) {
-  let { _id } = req.body;
-  const postToBeDeleted = await Post.findByIdAndDelete(_id);
-  if (postToBeDeleted) {
+  try {
+    let { userId } = req.user;
+    let { postId } = req.params;
+
+    let user = await User.findById(userId);
+    let post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ msg: "post not found" });
+    }
+
+    if (post.user.toString() !== userId) {
+      return res.status(403).json({ msg: "you cannot delete this post" });
+    }
+
+    let publicId = post.publicId[0];
+    console.log(publicId);
+    let resourceType = post.resourceType[0];
+    console.log(resourceType);
+
+    await cloudinary.uploader
+      .destroy(publicId, {
+        resource_type: resourceType,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    user.createdPosts = user.createdPosts.filter(
+      (id) => id.toString() !== postId,
+    );
+
+    await user.save();
+    await post.deleteOne({});
     return res.status(200).json("Post deleted successfully");
-  } else {
-    return console.log("Post not found");
+  } catch (err) {
+    return res.json({ msg: err.message });
   }
 }
 
