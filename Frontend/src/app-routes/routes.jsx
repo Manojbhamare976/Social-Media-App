700a5c2c7c7090b0f81a3d277928ed4927472501
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-11 13:47:39 +0530

    feat: add route to render user profile page component

--- Frontend/src/app-routes/routes.jsx
+++ Frontend/src/app-routes/routes.jsx
@@ -2,12 +2,14 @@ import Homepage from "../pages/Homepage.jsx";
 import CreatePost from "../pages/CreatePost.jsx";
 import CreateComment from "../pages/CreateComment.jsx";
 import SearchUser from "../pages/Search.jsx";
+import Userprofile from "../pages/Profilepage.jsx";
 
 const routes = [
   { path: "/", element: <Homepage /> },
   { path: "/post/create", element: <CreatePost /> },
   { path: "/comment", element: <CreateComment /> },
   { path: "/find/user", element: <SearchUser /> },
+  { path: "/userprofile/user", element: <Userprofile /> },
 ];
 
 export default routes;
