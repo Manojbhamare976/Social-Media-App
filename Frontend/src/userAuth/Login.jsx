4b932f7debd2ea79ad098d36f5167460640473c6
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-18 16:04:24 +0530

    feat: add form validation to login form

--- Frontend/src/userAuth/Login.jsx
+++ Frontend/src/userAuth/Login.jsx
@@ -1,34 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import api from "../api/axiosUserClient.js";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   let [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
+  let [loggedIn, setLoggedIn] = useState(false);
+  let [error, setError] = useState({ text: "" });
+  let [usernameError, setUsernameError] = useState({ text: "" });
+  let [passwordError, setPasswordError] = useState({ text: "" });
+  let [emailError, setEmailError] = useState({ text: "" });
 
   async function submitForm(e) {
     e.preventDefault();
-    let username = formData.username;
-    let email = formData.email;
+
+    setError({ text: "" });
+    setUsernameError({ text: "" });
+    setPasswordError({ text: "" });
+    setEmailError({ text: "" });
+
+    let username = formData.username.trim();
+    let email = formData.email.trim();
     let password = formData.password;
+
+    if (username.length <= 0) {
+      return setUsernameError({ text: "Please enter username" });
+    } else if (email.length <= 0) {
+      return setEmailError({ text: "Please enter email" });
+    } else if (password.length <= 0) {
+      return setPasswordError({ text: "Please enter password" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return setEmailError({
+        text: "Please enter a valid email address",
+      });
+    }
+
+    if (password.length < 6) {
+      return setPasswordError({
+        text: "password must contain atleast 6 characters",
+      });
+    }
+
     try {
-      api.post("http://localhost:3000/user/login", {
+      await api.post("http://localhost:3000/user/login", {
         username: username,
         email: email,
         password: password,
       });
-      console.log("User logged in successfully");
+      setLoggedIn(true);
+      return console.log("User logged in successfully");
     } catch (err) {
-      return err.message;
+      setError({ text: err.response?.data?.msg || "Something went wrong" });
     }
   }
+
+  useEffect(() => {
+    if (loggedIn) {
+      navigate("/", { replace: true });
+    }
+  }, [loggedIn, navigate]);
 
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={submitForm}>
           <input
             name="username"
             type="text"
@@ -38,6 +81,7 @@ export default function Login() {
               setFormData({ ...formData, [e.target.name]: e.target.value });
             }}
           />
+          {usernameError.text.length > 0 ? <p>{usernameError.text}</p> : null}
           <input
             name="email"
             type="email"
@@ -47,6 +91,7 @@ export default function Login() {
               setFormData({ ...formData, [e.target.name]: e.target.value });
             }}
           />
+          {emailError.text.length > 0 ? <p>{emailError.text}</p> : null}
           <input
             name="password"
             type="password"
@@ -56,7 +101,16 @@ export default function Login() {
               setFormData({ ...formData, [e.target.name]: e.target.value });
             }}
           />
-          <button onClick={submitForm}>Login</button>
+          {passwordError.text.length > 0 ? <p>{passwordError.text}</p> : null}
+          {loggedIn === true ? (
+            <button type="submit" disabled>
+              Login
+            </button>
+          ) : (
+            <button type="submit">Login</button>
+          )}
+
+          {error.text.length > 0 ? <p>{error.text}</p> : null}
         </form>
       </div>
     </>
