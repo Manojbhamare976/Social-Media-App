a7935eba6824e500c78460d3a1c8951666985818
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-20 13:33:49 +0530

    style: add style for MainLayout component

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../homepage-components/Sidebar.jsx";
-
+import "./MainLayout.css";
 export default function MainLayout() {
   return (
     <div className="homepage-layout">
