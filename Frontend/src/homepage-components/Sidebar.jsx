81eab5bb9e0e244b5082d6488043c19a7bc1a863
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-20 13:34:57 +0530

    style: add styles to sidebar component

--- Frontend/src/homepage-components/Sidebar.jsx
+++ Frontend/src/homepage-components/Sidebar.jsx
@@ -1,42 +1,41 @@
-import { useState } from "react";
+import "./Sidebar.css";
 import { House, Search, Plus, CircleUserRound } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import AppRoutes from "../app-routes/AppRoutes.jsx";
-
 export default function Sidebar() {
   const navigate = useNavigate();
 
   return (
     <>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        <House />
-      </button>
-      <button
-        onClick={() => {
-          navigate("/find/user");
-        }}
-      >
-        <Search />
-      </button>
-      <button
-        onClick={() => {
-          navigate("/post/create");
-        }}
-      >
-        <Plus />
-      </button>
-      <button
-        onClick={() => {
-          navigate("/userprofile/user");
-        }}
-      >
-        <CircleUserRound />
-      </button>
-      <AppRoutes />
+      <div className="sidebar">
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <House />
+        </button>
+        <button
+          onClick={() => {
+            navigate("/find/user");
+          }}
+        >
+          <Search />
+        </button>
+        <button
+          onClick={() => {
+            navigate("/post/create");
+          }}
+        >
+          <Plus />
+        </button>
+        <button
+          onClick={() => {
+            navigate("/userprofile/user");
+          }}
+        >
+          <CircleUserRound />
+        </button>
+      </div>
     </>
   );
 }
