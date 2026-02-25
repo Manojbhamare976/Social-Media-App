88df7d1c1a26baabd92b99f4c6b306109d13cff6
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-02-25 14:21:50 +0530

    feat: add get route for showFollowers controller

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -9,6 +9,7 @@ import {
   removeProfilePic,
   findUserProfile,
   getCurrentUserId,
+  showFollowers,
 } from "../Controllers/userController.js";
 import { authenticateUser } from "../Controllers/userAuth.js";
 const router = express.Router();
@@ -22,4 +23,5 @@ router.get("/userprofile/:userId", authenticateUser, findUserProfile);
 router.patch("/update/profile", authenticateUser, updateProfile);
 router.patch("/remove/profilepic", authenticateUser, removeProfilePic);
 router.get("/me/id", authenticateUser, getCurrentUserId);
+router.get("/followers", authenticateUser, showFollowers);
 export default router;
