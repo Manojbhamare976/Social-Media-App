96138925e556949b4a1a06560ae844f41397c010
Manoj Bhamare <bhamaremanoj115@gmail.com>
2026-03-03 15:40:23 +0530

    feat: add logout option in sidebar

--- Frontend/src/homepage-components/Sidebar.jsx
+++ Frontend/src/homepage-components/Sidebar.jsx
@@ -1,41 +1,62 @@
 import "./Sidebar.css";
-import { House, Search, Plus, CircleUserRound } from "lucide-react";
+import {
+  House,
+  Search,
+  Plus,
+  CircleUserRound,
+  EllipsisVertical,
+} from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../api/axiosUserClient";
+
 export default function Sidebar() {
   const navigate = useNavigate();
+  const [openMenu, setOpenMenu] = useState(false);
+
+  async function handleLogout() {
+    try {
+      await api.put("/user/logout");
+
+      alert("Logout successfull");
+      navigate("/login");
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setOpenMenu(false);
+    }
+  }
 
   return (
-    <>
-      <div className="sidebar">
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          <House />
-        </button>
-        <button
-          onClick={() => {
-            navigate("/find/user");
-          }}
-        >
-          <Search />
-        </button>
-        <button
-          onClick={() => {
-            navigate("/post/create");
-          }}
-        >
-          <Plus />
-        </button>
-        <button
-          onClick={() => {
-            navigate("/userprofile/user");
-          }}
-        >
-          <CircleUserRound />
-        </button>
+    <div className="sidebar">
+      <button onClick={() => navigate("/")}>
+        <House />
+      </button>
+
+      <button onClick={() => navigate("/find/user")}>
+        <Search />
+      </button>
+
+      <button onClick={() => navigate("/post/create")}>
+        <Plus />
+      </button>
+
+      <button onClick={() => navigate("/userprofile/user")}>
+        <CircleUserRound />
+      </button>
+
+      <div className="sidebar-menu-wrapper">
+        <EllipsisVertical
+          className="sidebar-menu-icon"
+          onClick={() => setOpenMenu((p) => !p)}
+        />
+
+        {openMenu && (
+          <div className="sidebar-dropdown">
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
